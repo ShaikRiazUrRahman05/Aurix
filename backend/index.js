9c21a2fa8cc6d5286369bb12eb7d647772b335ca
ShaikRiazUrRahman05 <127473684+ShaikRiazUrRahman05@users.noreply.github.com>
2025-07-02 15:09:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://aurix-your-virtual-assistant.onrender.com",
     credentials: true,
   })
 );
