47cf0015f0863364c32d191762d4a14e920dc4b3
Shaik Riaz Ur Rahman <127473684+ShaikRiazUrRahman05@users.noreply.github.com>
2026-07-28 13:00:14 +0530

    Updated Home.jsx

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -31,7 +31,7 @@ function Home() {
         withCredentials: true,
       });
       setUserData(null);
-      navigate("/sigin");
+      navigate("/signin");
     } catch (error) {
       console.log(error);
     }
